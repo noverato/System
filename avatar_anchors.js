a67119a3c23a01d119e4a9cfbdcd8036c5572b45
noverato <spawn-sn@arcor.de>
2026-01-29 10:18:25 +0100

    Update avatar_anchors.js

--- avatar_anchors.js
+++ avatar_anchors.js
@@ -1,72 +1,138 @@
 /**
- * THE NEST - AVATAR ANCHOR SYSTEM
- * Erstellt die visuelle Verbindung zwischen Avataren und Ausrüstung.
+ * THE NEST - AVATAR ANCHOR DATABASE
+ * 98 Avatare - Koordinaten-Mapping für Waffen und Schilde
  */
 
-// 1. FullLootPool global verfügbar machen (als flache Liste für schnellen Zugriff)
-window.allItems = {};
-
-(function prepareGlobalLoot() {
-    // Wir iterieren durch den komplexen FullLootPool aus loot.js
-    // Struktur: FullLootPool[Kategorie][Tier][Pfad]
-    for (const category in FullLootPool) {
-        for (const tier in FullLootPool[category]) {
-            ['light', 'dark'].forEach(path => {
-                FullLootPool[category][tier][path].forEach(item => {
-                    // Mapping nach Item-Name für inventar.js
-                    window.allItems[item.name] = item;
-                });
-            });
-        }
-    }
-    console.log(`[Anchor-System] ${Object.keys(window.allItems).length} Items in globalen Pool geladen.`);
-})();
-
-// 2. Zentrale Datenbank für Avatar-Ankerpunkte
-// Hier definierst du einmal pro Avatar die Offsets für Waffentypen.
 const AvatarAnchors = {
-    "Warrior_Male": {
-        "weapon": { x: 12, y: -5, rotation: 45 },
-        "shield": { x: -10, y: 5, rotation: 0 }
-    },
-    "Mage_Female": {
-        "weapon": { x: 8, y: -12, rotation: 90 },
-        "shield": { x: -8, y: 0, rotation: 0 }
-    },
-    // Hier werden die restlichen der 98 Avatare ergänzt...
-    "Default": {
-        "weapon": { x: 0, y: 0, rotation: 0 },
-        "shield": { x: 0, y: 0, rotation: 0 }
-    }
+    // --- SPEZIALFORMEN ---
+    "Das_Ei": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Der_Keimling": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- WALDLÄUFER (LICHT) ---
+    "Licht_Novize": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Pfadfinder_des_Glanzes": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Licht_Bogenschuetze": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Scharfschuetze_des_Hains": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Elite_Waidmann": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Meister_Wildlaeufer": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Licht_Schuetze": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- ASSASSINE (WALDLÄUFER DUNKEL) ---
+    "Schatten_Striezi": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Nacht_Schleicher": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Gift_Dolch_Adept": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Hinterhalt_Assassine": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Schatten_Viper": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Meister_der_lautlosen_Klinge": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Nachtschatten_Assassine": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- NATUR-KRIEGER (LICHT) ---
+    "Wald_Rekrut": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Gruen_Gardist": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Elfen_Krieger": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Silberblatt_Kaempfer": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Elite_Hueter": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Licht_Champion": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Ewiger_Licht_Vanguard": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- NATUR-KRIEGER (DUNKEL) ---
+    "Schatten_Fechter": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Nacht_Klinge": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Finster_Duellant": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Obsidian_Stecher": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Elite_Exekutor": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Fuerst_der_schwarzen_Klingen": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Schatten_Vanguard": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- DRUIDE (LICHT) ---
+    "Mystischer_Novize": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Rufer_des_Wachstums": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Licht_Alchemist": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Hueter_der_Lebensquelle": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Astral_Schamane": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Hoher_Druide_des_Lichts": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Ewiger_Smaragd_Erzmagier": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- DRUIDE (DUNKEL) ---
+    "Schatten_Adept": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Sporen_Waver": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Verderber_des_Gruens": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Parasiten_Meister": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Nachtmahr_Schamane": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Hoher_Exekutor_der_Faeulnis": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Leeren_Botaniker": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- HÜTER (LICHT) ---
+    "Nest_Wache": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Pfad_Verteidiger": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Schild_Veteran": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Licht_Garde_Hueter": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Meister_der_Verteidigung": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Silber_Bollwerk": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Ewiger_Nest_Protektor": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- HÜTER (DUNKEL) ---
+    "Verstossener_Waechter": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Gram_Traeger": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Schatten_Eisenschild": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Gebrochener_Verteidiger": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Bollwerk_des_Leids": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Finstere_Nest_Gewalt": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Schild_Tyrann": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- SUCHER (LICHT) ---
+    "Gassen_Laeufer": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Pfad_Entdecker": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Licht_Vagabund": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Meister_der_Akrobatik": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Schatzjaeger_des_Hains": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Hueter_der_Verborgenheit": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Gluecksbringer": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- SUCHER (DUNKEL) ---
+    "Schatten_Stricher": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Nacht_Taschendieb": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Klingen_Beutelschneider": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Hinterhalt_Pluenderer": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Dunkler_Schlitzohr_Meister": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Schatten_Plage_des_Nests": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Meisterdieb": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- EINSIEDLER (LICHT) ---
+    "Gelehrten_Lehrling": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Sucher_der_Wahrheit": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Licht_Chronist": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Hoher_Gebieter_der_Reinheit": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Medium_des_heiligen_Hains": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Licht_Orakel": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Licht_Avatar_der_Weisheit": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- EINSIEDLER (DUNKEL) ---
+    "Schatten_Schueler": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Rufer_der_Finsternis": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Schwarz_Magier": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Lord_der_Gravitation": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Gebieter_des_schwarzen_Lochs": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Schatten_Eminenz": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Exekutor_der_Leere": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- WÄCHTER (LICHT) ---
+    "Heiliger_Anwaerter": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Licht_Gardist": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Ritter_des_Morgengrauens": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Heiliger_Grossritter": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Klinge_der_Gerechtigkeit": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Goldener_Loewen_Waechter": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Licht_Souveraen": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    // --- WÄCHTER (DUNKEL) ---
+    "Schatten_Anwaerter": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Dunkler_Pfadfinder": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Ritter_des_Abgrunds": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Schatten_Kommandant": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Gebieter_der_Finsternis": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Monarch_Anwaerter": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+    "Legendaerer_Schatten_Souveraen": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } },
+
+    "Default": { weapon: { x: 0, y: 0, rotation: 0 }, shield: { x: 0, y: 0, rotation: 0 } }
 };
-
-/**
- * 3. Injektions-Funktion
- * Speist die Anker-Daten in das 'offsets' Feld jedes Items ein.
- */
-function injectAvatarAnchors() {
-    const allAvatarIDs = Object.keys(AvatarAnchors);
-    let injectionCount = 0;
-
-    for (const itemName in window.allItems) {
-        const item = window.allItems[itemName];
-        
-        // Wir gehen jeden Avatar durch und setzen den passenden Slot-Offset
-        allAvatarIDs.forEach(avatarID => {
-            const slot = item.slot; // "weapon" oder "shield"
-            
-            if (AvatarAnchors[avatarID] && AvatarAnchors[avatarID][slot]) {
-                // Weise dem Item den spezifischen Offset für diesen Avatar zu
-                item.offsets[avatarID] = AvatarAnchors[avatarID][slot];
-            } else {
-                // Fallback auf Default, falls Avatar/Slot nicht definiert
-                item.offsets[avatarID] = AvatarAnchors["Default"][slot] || { x:0, y:0, rotation:0 };
-            }
-        });
-        injectionCount++;
-    }
-    console.log(`[Anchor-System] Offsets für ${injectionCount} Items erfolgreich injiziert.`);
-}
-
-// Ausführung der Injektion beim Laden
-injectAvatarAnchors();
